f9925d40f3f912f8040ef09184b52b96d2cde79f
Shashank Lal <shashanklal01@gmail.com>
2020-12-20 13:47:43 +0400

    scrape lyrics from url

--- program.py
+++ program.py
@@ -48,3 +48,15 @@ def request_song_url(artist_name, song_cap):
         else:
             page += 1
     return songs
+
+
+# Scrape lyrics from a Genius.com song URL
+def scrape_song_lyrics(url):
+    page = requests.get(url)
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics = html.find('div', class_='lyrics').get_text()
+    # remove identifiers like chorus, verse, etc
+    lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+    # remove empty lines
+    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
+    return lyrics
